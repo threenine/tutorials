86232eb889422327adf07e9bbb58c79682a86c1e
Gary Woodfine <lnb0l9dc@duck.com>
2024-05-16 20:50:56 +0100

    Restructure test projects and update launch settings
    Reorganized the test structure by separating integration and unit tests into their respective projects. The ServerTest project was renamed to Integration and a new Unit test project was added. Also, the launchBrowser setting in launchSettings.json was set to false to prevent the browser from opening automatically at launch.

--- WebSockets/Server/src/Program.cs
+++ WebSockets/Server/src/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Server;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<Notifier>();
---
+++ WebSockets/Server/tests/unit/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
